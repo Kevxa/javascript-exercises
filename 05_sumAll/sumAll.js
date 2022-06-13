ce0e477f983fd381c08c4e581ba58d7b3d680886
Kevin Feuerstein <k.feuerstein1@gmail.com>
2022-06-13 10:34:29 +0200

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // wenn negative zahlen oder keine zahlen sind kommt error message
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
 
-};
+    // wenn die erste zahl größer ist wird sie vertauscht und zu "max"
+    if (min > max){
+        const temp = min;
+        min = max;
+        max = temp;
+
+    }
+    // Das hier allein is sumall
+    let sum = 0;
+    for(let i = min; i <= max; i++){
+        sum += i;
+
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(fahren) {
+  let sum = (fahren - 32) * 5/9;
+  let roundsum = Math.round(sum * 10)/ 10;
+  
+  return roundsum;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let sum = celcius * 9/5 + 32 ;
+  let roundsum = Math.round(sum * 10)/ 10;
+  return roundsum;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
+
 };
